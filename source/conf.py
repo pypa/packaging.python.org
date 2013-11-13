1e3e1631a63d6f14e973b03263d674e49295b3fb
Marcus Smith <qwcode@gmail.com>
2013-11-13 15:43:48 -0800

    switch to new 'default' theme for RTD; locally, use it as 'sphinx_rtd_theme' if it's available

--- source/conf.py
+++ source/conf.py
@@ -13,6 +13,8 @@
 
 import sys, os, glob
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -25,7 +27,7 @@ import sys, os, glob
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['themes/pug/templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -91,7 +93,14 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'pydoctheme'
+html_theme = 'default'
+if not on_rtd:
+    try:
+        import sphinx_rtd_theme
+        html_theme = 'sphinx_rtd_theme'
+    except ImportError:
+        pass
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -99,7 +108,13 @@ html_theme = 'pydoctheme'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['themes']
+html_theme_path = []
+if not on_rtd:
+    try:
+        import sphinx_rtd_theme
+        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    except ImportError:
+        pass
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -120,7 +135,7 @@ html_theme_path = ['themes']
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['themes/pug/static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -131,12 +146,7 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-paths = glob.glob(os.path.join(os.path.dirname(__file__), '*.rst'))
-sidebars = [(os.path.splitext(os.path.basename(path))[0], 'sidebar.html') for path in paths]
-# NOTE: using the glob syntax for "html_sidebars" that sphinx>1.0 supports won't
-# work here, because it apparently fails to set the sourcename variable that our
-# "sidebar.html" template needs
-html_sidebars = dict(sidebars)
+html_sidebars = {}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
