18705827b110192460cc1edcb7edbcbfb8b62ad3
Jon Wayne Parrott <jonwayne@google.com>
2017-05-25 18:01:14 -0700

    Move pypa theme into its own package

---
+++ pypa-theme/README.rst
@@ -0,0 +1,5 @@
+PyPA Sphinx Theme
+=================
+
+This theme is the common theme for all PyPA projects. This theme is an
+externalized version of Python 3's pydoctheme.
--- source/conf.py
+++ source/conf.py
@@ -122,7 +122,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydoctheme'
+html_theme = 'pypa_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -133,7 +133,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['themes']
+# html_theme_path = ['themes']
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
