337f959d40fbf2efaced750c28b334aaf02ca422
meowmeowcat <68463158+meowmeowmeowcat@users.noreply.github.com>
2021-06-28 11:15:03 +0800

    Add Translatable Files

--- source/conf.py
+++ source/conf.py
@@ -83,7 +83,7 @@ release = ''
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 locale_dirs = ['locales/']
-
+language = None
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
 #
