de211e5065e11c25f82452315e45963eb7cfc2dc
meowmeowcat <68463158+meowmeowmeowcat@users.noreply.github.com>
2021-06-28 09:45:19 +0800

    Add locale_dirs in conf.py

--- source/conf.py
+++ source/conf.py
@@ -82,7 +82,7 @@ release = ''
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+locale_dirs = ['locales/']
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
