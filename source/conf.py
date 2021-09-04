b018b14285e795950f8e473cc9ac617915ce69f4
meowmeowcat <meowmeowcat1211@gmail.com>
2021-09-04 14:30:28 +0800

    Remove an empty line
    Co-authored-by: Sviatoslav Sydorenko <webknjaz@redhat.com>

--- source/conf.py
+++ source/conf.py
@@ -383,7 +383,6 @@ extlinks = {
     'user': (f'{github_sponsors_url}/%s', '@'),  # noqa: WPS323
 }
 
-
 linkcheck_ignore = [
     "http://localhost:\d+",
     # Ignoring these links as they will redirect to login page if folks haven't logged in.
